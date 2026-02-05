ab1a25e80a30635863e855e53516e78b33d55e74
TiaraChontesa <d1041231070@student.untan.ac.id>
2026-02-05 16:31:33 +0700

    first commit

---
+++ KodePertama.java
@@ -0,0 +1,8 @@
+public class KodePertama {
+    public static void main(String[] args) {
+        System.out.println("Nama            : Tiara Chontesa");
+        System.out.println("NIM             : D1041231070");
+        System.out.println("Semester        : 6");
+        System.out.println("Prodi           : Informatika");
+    }
+}
