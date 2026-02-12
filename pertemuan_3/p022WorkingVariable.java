0a07b04f0b3811af014d64540e6998638ffe5037
TiaraChontesa <d1041231070@student.untan.ac.id>
2026-02-12 14:04:56 +0700

    Add files via upload

---
+++ pertemuan_3/p022WorkingVariable.java
@@ -0,0 +1,15 @@
+public class p022WorkingVariable {
+    public static void main(String[] args) {
+        //Deklarasi dan Inisialisasi Variable
+        int angka = 10;
+
+        //Menampilkan nilai awal variable
+        System.out.println("Nilai awal angka : " + angka);
+
+        //Mengubah nilai variabel
+        angka = 25;
+
+        //menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
---
+++ pertemuan_3/p023PrimitiveData.java
@@ -0,0 +1,25 @@
+public class p023PrimitiveData {
+    public static void main(String[] args) {
+        // Tipe data Bilangan Bulat
+        int umur = 20;
+        long populasi = 8000000000L;
+
+        // Tipe Data Bilangan Desimal
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+
+        //Tipe data karakter
+        char nilaiHuruf = 'A';
+
+        //Tipe data logika
+        boolean lulus = true;
+
+        //menampilkan nilai semua variable
+        System.out.println("Umur            : " + umur);
+        System.out.println("Populasi        : " + populasi);
+        System.out.println("Tinggi Badan    : " + tinggi);
+        System.out.println("IPK             : " + ipk);
+        System.out.println("Nilai Huruf     : " + nilaiHuruf);
+        System.out.println("Status Lulus    : " + lulus);
+    }
+}
