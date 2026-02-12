0cdbf0d8016a7db5dea5d52f078e7ec9f00f39a2
TiaraChontesa <d1041231070@student.untan.ac.id>
2026-02-12 14:09:02 +0700

    Add files via upload

---
+++ Pertemuan_1/KodePertama_Latihan1.java
@@ -0,0 +1,8 @@
+public class KodePertama_Latihan1 {
+    public static void main(String[] args) {
+        System.out.println("Nama            : Tiara Chontesa");
+        System.out.println("NIM             : D1041231070");
+        System.out.println("Semester        : 6");
+        System.out.println("Prodi           : Informatika");
+    }
+}
