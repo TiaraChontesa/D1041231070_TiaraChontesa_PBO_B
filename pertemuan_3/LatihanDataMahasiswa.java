693a929820e8b887f0af4dbf40935ea5e3a16f1c
TiaraChontesa <d1041231070@student.untan.ac.id>
2026-02-13 08:26:39 +0700

    commit
    Menambahkan Latihan 1 pada Pertemuan 3

---
+++ pertemuan_3/LatihanDataMahasiswa.java
@@ -0,0 +1,30 @@
+public class LatihanDataMahasiswa {
+    public static void main(String[] args) {
+        //Tipe data long
+        long nim = 2301010001L;
+
+        //Tipe data string
+        String nama = "Budi Santoso";
+
+        //tipe data char
+        char kelas = 'A';
+
+        //tipe data boolean
+        boolean aktif = true;
+
+        //tipe data double
+        double ipk = 3.75;
+
+        //tipe data int
+        int totalSks = 72;
+
+        //Menampilkan output
+        System.out.println("=== DATA MAHASISWA ===");
+        System.out.println("NIM             : " + nim);
+        System.out.println("Nama            : " + nama);
+        System.out.println("Kelas           : " + kelas);
+        System.out.println("Status Aktif    : " + aktif);
+        System.out.println("IPK Semester    : " + ipk);
+        System.out.println("Total SKS       : " + totalSks);
+    }
+}
