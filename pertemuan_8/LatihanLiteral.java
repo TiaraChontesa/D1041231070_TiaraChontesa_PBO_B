a182661ba12f1bf4204e02929c0c4fe14ef0f7b2
TiaraChontesa <d1041231070@student.untan.ac.id>
2026-02-26 15:54:08 +0700

    commit
    Menambahkan LatihanCariKelipatan, LatihanLiteral, LatihanTotalGenap dan LatihanMenuHarian pada pertemuan 8

---
+++ pertemuan_8/LatihanLiteral.java
@@ -0,0 +1,37 @@
+public class LatihanLiteral {
+    public static void main(String[] args) {
+        System.out.println("=== DEMONSTRASI LITERAL TIPE DATA ===");
+
+        //tipe data byte
+        byte maksimum = 127;
+        System.out.println("Tipe byte           : " + maksimum);
+
+        //tipe data short
+        short tahun = 2026;
+        System.out.println("Tipe short          : " + tahun);
+
+        //tipe data int
+        int populasiind = 275000000;
+        System.out.println("Tipe int            : " + populasiind);
+        
+        //tipe data long
+        long populasidunia = 8000000000L;
+        System.out.println("Tipe long           : " + populasidunia);
+
+        //tipe data float 
+        float phi = 3.14f;
+        System.out.println("Tipe float          : " + phi);
+
+        //tipe data double
+        double nilaiE = 2.718281828;
+        System.out.println("Tipe double         : " + nilaiE);
+
+        //tipe data char
+        char hurufpertama = 'B';
+        System.out.println("Tipe char           : " + hurufpertama);
+
+        //tipe data boolean;
+        boolean status = true;
+        System.out.println("Tipe boolean        : " + status);
+    }
+}
